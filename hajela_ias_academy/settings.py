81daa7345215a330ed171da8754a803b4bdf3a6a
Kamlesh Lovewanshi <kamleshlovewanshi2000@gmail.com>
2024-08-29 17:22:20 +0530

    add social media links and update setting for production

--- hajela_ias_academy/settings.py
+++ hajela_ias_academy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cyxkx2re60)80#%z7nlhyo8u)x*4=ve5gk*a56s%hdc8ow&!3w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['hicareers.org', 'www.hicareers.org']
 
 
 # Application definition
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
